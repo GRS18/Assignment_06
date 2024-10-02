6d2b57f91a7fd35c55e86db0c5d87d12016539c3
Girish Ravindra Shinde <104589353+GRS18@users.noreply.github.com>
2024-10-03 00:06:46 +0530

    Update CarEngine.java

--- CarEngine.java
+++ CarEngine.java
@@ -1,9 +1,59 @@
 package Assignment_02;
 
+class Engine {
+    private String engineName;
+    private int engineNumber;
+
+    public Engine(String engineName, int engineNumber) {
+        this.engineName = engineName;
+        this.engineNumber = engineNumber;
+    }
+
+    public void displayEngine() {
+        System.out.println("Name of Engine : " + engineName);
+        System.out.println("Number of Engine : " + engineNumber);
+    }
+}
+
+class Car extends Engine {
+    private String carName;
+    private String color;
+
+    public Car(String engineName, int engineNumber, String carName, String color) {
+        super(engineName, engineNumber);
+        this.carName = carName;
+        this.color = color;
+    }
+
+    public void displayCar() {
+        System.out.println("Car name : " + carName);
+        System.out.println("Color : " + color);
+    }
+
+    public void startCar() {
+        System.out.println("Car Started");
+    }
+
+    public void stopCar() { 
+        System.out.println("Car Stopped");
+    }
+}
+
 public class CarEngine {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		 Engine engine = new Engine("Electric", 555);
+	        engine.displayEngine();
+	 
+	        System.out.println("------------------------------");
+
+	        Car car = new Car("Flat",2856, "Audi", "Black");
+	        car.displayCar();
+	        System.out.println("-------------------------------");
+	        car.startCar();
+	        car.stopCar(); 
+		
 
 	}
 
